ff4a10c18adb6762ffca497d2eb12d566316f434
Mugil005 <146934544+Mugil005@users.noreply.github.com>
2024-08-27 20:50:13 +0530

    Add files via upload

---
+++ Test Codes/exception.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class exception {
+	public static void main(String[] args) {
+		int a = 30, b = 0;
+		int c = 0;	
+		try {
+			c = a/b;
+			System.out.println(c);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Exception occured");	
+		}
+		catch (NullPointerException e) {
+			System.out.println("Index Not found");
+		}
+		finally {
+			int fact = 1;
+			for (int i =1;i<=5;i++) {
+				fact = fact*i;	
+			}
+			System.out.println(fact);
+		}
+		System.out.println(c);
+		System.out.println("End of the program");
+	}
+}
