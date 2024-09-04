00defe42e747568d4077e4e4a6ddfdd300066281
Mugil005 <146934544+Mugil005@users.noreply.github.com>
2024-09-04 21:32:20 +0530

    Add files via upload

---
+++ Java Basics/arithematic.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class s {
+    public static void main (String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+          
+    }
+}
